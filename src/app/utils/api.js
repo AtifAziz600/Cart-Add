98dab23ff43de85bbbea150343b08d5f7f57ff78
AtifAziz600 <feralligatrfahim630@gmail.com>
2024-07-16 15:44:57 +0600

    'g'

--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -1,4 +1,4 @@
-// pages/api/products.js
+
 
 const products = [
   [
@@ -35,39 +35,17 @@ const products = [
 
 export default function handler(req, res) {
   if (req.method === "GET") {
-    // Handle GET request
-    res.status(200).json(products);
+       res.status(200).json(products);
   } else if (req.method === "POST") {
-    // Handle POST request
-    const newProduct = req.body; // Assuming JSON request body
+    
+    const newProduct = req.body; 
     products.push(newProduct);
     res.status(201).json(products);
   } else {
-    // Handle other HTTP methods
+    
     res.setHeader("Allow", ["GET", "POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
 
-// import axios from "axios";
 
-// const API_KEY = "ed3fcd7d26mshc37bdd7c1db6785p1e4674jsn76d6955181bc";
-// const BASE_URL = "http://rapidapi.com/products";
-
-// export const fetchProducts = async () => {
-//   try {
-//     const response = await axios.get(`${BASE_URL}/products`, {
-//       headers: {
-//         "x-rapidapi-key": API_KEY,
-//         "x-rapidapi-host":
-//           "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-//       },
-//     });
-//     return response.data;
-//   } catch (error) {
-//     console.error("Error fetching products:", error);
-//     return null;
-//   }
-// };
-
-// export default fetchProducts;
